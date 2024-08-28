f69646a5a90e4f8c2685c1ab09064640f20eb5e2
Karen Tian <9136901+karentian@users.noreply.github.com>
2024-08-28 09:47:33 -0400

    update fft

--- kt_fft.m
+++ kt_fft.m
@@ -1,4 +1,4 @@
-function [f amp] = kt_fft(data)
+function [f amp P1] = kt_fft(data,plotFigs)
 % function [f P1] = kt_fft(data)
 
 % Inputs: 
@@ -7,28 +7,29 @@ function [f amp] = kt_fft(data)
 %   - f: frequencies (Hz) 
 %   - amp: power
 
-%% Directories (for figure saving) 
-s.saveFigs = 1; 
-s.figDir = '/Users/karen/Dropbox/github/ta-esoa/ta-esoa_analysis/figs/timing0001_KT'; 
-s.figType = 'png'; 
-
-addpath(genpath('../kt-utils')); 
 
 %% FFT setup 
 % Need at least 2 points to estimate a sin
 % If N is the number of points in a signal 
 % N/2 is the maximum frequency that can be sampled (i.e. Nyquist frequency)
 
-data = dat'; % check input dimensions 
+% if size(data,1)==1
+%     data = data'; % check input dimensions
+% end
+
+if nargin<2
+    plotFigs = 1; 
+end
 
 %% Check data
 nSamples = length(data); 
 
 %% Zero pad input data 
 clear nSamplesDesired dataOriginal padSize dataPadded
-nSamplesDesired = 2^nextpow2(length(data)); % next power of 2 makes for more efficient algorithm 
+% nfft = 2^nextpow2(length(data)); % next power of 2 makes for more efficient algorithm 
+nfft = length(data); 
 dataOriginal = data; 
-padSize = 2^nextpow2(length(data)) - nSamples; % 0, 1024*2-numel(dataOriginal); 
+padSize = nfft - nSamples; % 0, 1024*2-numel(dataOriginal); 
 dataPadded = padarray(dataOriginal,[padSize 1],0,'post');
 
 %% Setup 
@@ -36,9 +37,9 @@ dataPadded = padarray(dataOriginal,[padSize 1],0,'post');
 Fs = 1000;                      % Sampling frequency                    
 T = 1/Fs;                       % Sampling period       
 L = length(dataPadded);         % Length of signal
-y = fft(dataPadded);            % FFT the signal -- 2-sided frequency, complex
+y = fft(dataPadded,nfft);            % FFT the signal -- 2-sided frequency, complex
 P2 = abs(y/L);                  % 2-sided PSD -- 2-sided frequency, real magnitude
-P1 = P2(1:L/2+1);               % 1-sided PSD -- 1-sided frequency, real magnitude
+P1 = P2(1:floor(L/2+1));        % 1-sided PSD -- 1-sided frequency, real magnitude
 P1(2:end-1) = 2*P1(2:end-1);    % Power, scaled to match time series magnitude (*)
 df = Fs/length(dataPadded);     % frequency resolution 
 f = Fs*(0:(L/2))/L;             % frequencies 
@@ -48,9 +49,10 @@ amp = P1;
 
 %% Plot settings
 color = [92 107 192]/255; 
-xlims = [0 300]; 
+xlims = [0 150]; 
 % Anything to call out? Will draw vertical lines at these frequencies 
-fAnnotate = [20 120]; % ssver freq, line noise 
+fAnnotate = [10 20 120]; % ssver freq, line noise 
+% ylims = [0 200]; 
     
 %% Plot fft 
 if plotFigs
@@ -58,11 +60,14 @@ if plotFigs
     subplot 121
     hold on
     meg_figureStyle
-    plot(f,P1,'LineWidth',2,'Color',color)
+    plot(f(2:end),P1(2:end),'LineWidth',2,'Color',color)
     ylabel('Amplitude')
     xlabel('Frequency (Hz)')
     xlim(xlims)
-    xline(fAnnotate)
+    % ylim(ylims)
+    for iA = 1:numel(fAnnotate)
+        xline(fAnnotate(iA),'label',sprintf('%d Hz',fAnnotate(iA)))
+    end
 
     subplot 122
     hold on
@@ -71,7 +76,6 @@ if plotFigs
     ylabel('Log(Amplitude)')
     xlabel('Log(Frequency) (Hz)')
     xlim(log(xlims))
-    
-    kt_saveFigs('FFT_timing0001_KT', s)
+    xline(log(fAnnotate))
 end
 
